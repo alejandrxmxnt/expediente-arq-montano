7b6e8b21694e52232c395bbb2139a051bff5e946
alejandrxmxnt <amont9483@gmail.com>
2026-09-12 12:15:13 -0400

    Creacion de codigo base implementando principios SOLID

---
+++ H3/Base/base.js
@@ -0,0 +1,46 @@
+//Sistema de ventas con inventario
+class Rol {
+    // # es una forma de definir que va ser de aspecto privado por lo tanto esto es un encapsulamiento
+    #idRol;
+    #nombre;
+    #descripcion;
+    #estado;
+    #fechaCreacion;
+
+    constructor (idRol, nombre, descripcion){
+        this.#idRol = idRol;
+        this.#nombre = nombre;
+        this.#descripcion = descripcion;
+        this.#estado = true;
+        this.#fechaCreacion = new Date();
+    }
+
+    get idRol (){
+        return this.#idRol;
+    }
+    get nombre(){
+        return this.#nombre;
+    }
+    get descripcion() {
+        return this.#descripcion;
+    }
+    get estado () {
+        return this.#estado;
+    }
+    get fechaCreacion(){
+        return this.#fechaCreacion;
+    }
+
+    cmabiarEstado () {
+        this.#estado = !this.#estado;
+    }
+    editarEstado (nombre, descripcion) {
+        if(nombre&&nombre.trim() !== ""){
+            this.#nombre = nombre;
+        }
+        if(descripcion&&descripcion.trim() !== ""){
+            this.#descripcion = descripcion;
+        }
+    }
+}
+
